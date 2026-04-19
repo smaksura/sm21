fba3fbf889098e4d71f4756baa094cb9549b0f82
smaksura <sumaiya.smaksura@gmail.com>
2026-04-19 16:30:42 +0600

    FOUR

--- cls7/script.js
+++ cls7/script.js
@@ -1,4 +1,14 @@
 let num = 5;
-num > 4 ? console.log("num is greater than 4") : console.log("num is less than 4"); 
+num>4? console.log("num is greater than 4 "):console.log("num is less than 4");
+
+//const numbers = [10,20,30,40,50,60,70];
+//let [a,b,c,d,...restnumbers] = numbers;
+//console.log(a,b,c,d,restnumbers);
+
+let numbers = [1,2,3,4,5,6,7]
+let evennumber = numbers.filter(number => number %2==0);
+console.log(evennumber);
+let pownumber = evennumber.map(number =>Math.pow (number,2));
+console.log(pownumber);
 
 

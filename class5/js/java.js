6bc1355be207dc3246fc29e7de8ab86a8306b792
smaksura <sumaiya.smaksura@gmail.com>
2026-04-19 14:38:37 +0600

    third commit

---
+++ class5/js/java.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+let name = "IICT DUET";
+let age = 10;
+let isstudent = true;
+
+
+
+console.log("name:")
+console.log()
+console.log()
+console.log()
---
+++ cls7/script.js
@@ -0,0 +1,4 @@
+let num = 5;
+num > 4 ? console.log("num is greater than 4") : console.log("num is less than 4"); 
+
+

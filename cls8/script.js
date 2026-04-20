5a0b7599b5259a39207185aac9655746f73e06ad
smaksura <sumaiya.smaksura@gmail.com>
2026-04-20 16:34:27 +0600

    20/4

--- cls8/script.js
+++ cls8/script.js
@@ -11,3 +11,21 @@ console.log(products);
 const new_products = JSON.parse(products);
 console.log(new_products);
 
+function blockingTask(){
+    console.log("start");
+    setTimeout(()=>{
+        console.log("Async end");
+    },3000);
+       setTimeout(() => 
+     {console.log("end")},5000 );
+}
+
+ blockingTask(),
+ console.log("After task");
+
+let users
+const url = "https://jsonplaceholder.typicode.com/";
+fetch("https://jsonplaceholder.typicode.com/")
+.then( res => res.json() )
+.then(json => console.log(json))
+
